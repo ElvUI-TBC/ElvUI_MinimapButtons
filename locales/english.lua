96fe1a56e50ea8151e2891fa86b89fca3b5a68d9
Crum <stschleyer89@gmail.com>
2017-06-25 17:40:26 -0500

    init

---
+++ locales/english.lua
@@ -0,0 +1,6 @@
+﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local L = AceLocale:NewLocale("ElvUI", "enUS", true);
+if not L then return; end
+
+L["Inside Minimap"] = true;
+L["Minimap Button Grabber"] = true;
---
+++ locales/russian.lua
@@ -0,0 +1,6 @@
+﻿local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
+local L = AceLocale:NewLocale("ElvUI", "ruRU")
+if not L then return; end
+
+L["Inside Minimap"] = true;
+L["Minimap Button Grabber"] = true;
